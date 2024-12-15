136c5cf1ebcf075154569b1874a10c2aee67a433
Ashley-Wilson1 <ashley03wilson@gmail.com>
2024-12-15 14:10:18 +0000

    Added main and products pages for react front end

--- react-crud/src/App.tsx
+++ react-crud/src/App.tsx
@@ -3,6 +3,9 @@ import logo from "./logo.svg";
 import "./App.css";
 import Nav from "./components/Nav";
 import Menu from "./components/Menu";
+import Products from "./admin/Products";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from "./main/Main";
 
 function App() {
 	return (
@@ -12,30 +15,14 @@ function App() {
 			<div className="container-fluid">
 				<div className="row">
 					<Menu />
+
 					<main className="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-						<h2>Section title</h2>
-						<div className="table-responsive small">
-							<table className="table table-striped table-sm">
-								<thead>
-									<tr>
-										<th scope="col">#</th>
-										<th scope="col">Header</th>
-										<th scope="col">Header</th>
-										<th scope="col">Header</th>
-										<th scope="col">Header</th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<td>1,001</td>
-										<td>random</td>
-										<td>data</td>
-										<td>placeholder</td>
-										<td>text</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
+						<BrowserRouter>
+							<Routes>
+								<Route path="/admin/products" Component={Products} />
+								<Route path="/" Component={Main} />
+							</Routes>
+						</BrowserRouter>
 					</main>
 				</div>
 			</div>
